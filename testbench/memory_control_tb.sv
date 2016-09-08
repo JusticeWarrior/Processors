33afbdbfac5a934aea748fd0a43b3275623b074a
Jordan Huffaker <jhuffak@purdue.edu>
2016-09-08 14:02:21 -0400

    Now dumps memory to file

--- testbench/memory_control_tb.sv
+++ testbench/memory_control_tb.sv
@@ -34,9 +34,17 @@ module memory_control_tb;
   caches_if cif1();
   cache_control_if #(.CPUS(1)) ccif (cif0, cif1);
   // test program
-  test PROG ();
+  test PROG (CLK,nRST,ccif);
   // DUT
   memory_control DUT(CLK, nRST, ccif);
+
+
+endmodule
+
+program test(input logic CLK, output logic nRST, cache_control_if.cc ccif);
+  import cpu_types_pkg::*;
+
+  parameter PERIOD = 10;
 
   initial begin
     nRST = 1'b0;
@@ -148,10 +156,51 @@ module memory_control_tb;
 		$display("9 DIDNT SET dwait VALUE CORRECTLY");
 
 	$display("ALL TESTS FINISHED!");
+	dump_memory();
 
   end
 
-endmodule
 
-program test;
+  task automatic dump_memory();
+    string filename = "memcpu.hex";
+    int memfd;
+
+    //syif.tbCTRL = 1;
+    ccif.ramaddr = 0;
+    ccif.ramWEN = 0;
+    ccif.ramREN = 0;
+
+    memfd = $fopen(filename,"w");
+    if (memfd)
+      $display("Starting memory dump.");
+    else
+      begin $display("Failed to open %s.",filename); $finish; end
+
+    for (int unsigned i = 0; memfd && i < 16384; i++)
+    begin
+      int chksum = 0;
+      bit [7:0][7:0] values;
+      string ihex;
+
+      ccif.ramaddr = i << 2;
+      ccif.ramREN = 1;
+      repeat (4) @(posedge CLK);
+      if (ccif.ramload === 0)
+        continue;
+      values = {8'h04,16'(i),8'h00,ccif.ramload};
+      foreach (values[j])
+        chksum += values[j];
+      chksum = 16'h100 - chksum;
+      ihex = $sformatf(":04%h00%h%h",16'(i),ccif.ramload,8'(chksum));
+      $fdisplay(memfd,"%s",ihex.toupper());
+    end //for
+    if (memfd)
+    begin
+      //syif.tbCTRL = 0;
+      ccif.ramREN = 0;
+      $fdisplay(memfd,":00000001FF");
+      $fclose(memfd);
+      $display("Finished memory dump.");
+    end
+  endtask
 endprogram
